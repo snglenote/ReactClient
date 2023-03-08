deb3e1dd1c46fd2e9f069bde265b1c8bf70bc6ef
Calvine Akoonyatse <cakoonyatse@gmail.com>
2023-03-08 11:28:04 +0200

    created home and hero components

--- mirrormaxx/src/App.js
+++ mirrormaxx/src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import api from './api/axiosConfig';
 import { useState, useEffect } from 'react';
+import Layout from './components/Layout';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
 
@@ -32,6 +35,14 @@ function App() {
 
   return (
     <div className="App">
+
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<Home movies = {movies} />} ></Route>
+
+
+        </Route>
+      </Routes>
 
     </div>
   );
